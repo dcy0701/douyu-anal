bb58ec5f71f92296f3de499a92060d246ba63968
dcy0701 <979325848@qq.com>
2016-12-19 21:28:18 +0800

    Update index.js
    (づ￣ 3￣)づ

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import fetch from 'node-fetch'
-
 const depUrl = 'https://www.douyu.com/directory/all?isAjax=1&page='
 
-Array.from(Array(50/5)).reduce((pre,value,page)=>{
-    return pre.then(()=>{
-        return
-    })
-},Promise.reslove())
-// 先获取总的页数  暂定50页
+let count = 0;
 
-async fetchCount(page){
+(async function(){
+  await Array.from(Array(50)).reduce((pre,value,page)=>{
+      return pre.then(()=>{
+          return fetchCount(page+1)
+      })
+  },Promise.resolve())
+
+  console.log(count)
+})()
+
+async function fetchCount(page){
     const metadata = await fetch(depUrl+page)
     const pagedata = await metadata.text()
+    const outnumber = pagedata.match(/"dy-num fr"\s*\>[\d\.]*万*/g);
+
+    const all = outnumber
+    .map((value,index)=>{
+      value = value.replace('万','000').replace('.','')
+      return parseInt(value.slice(value.lastIndexOf('>')+1))
+    })
+    .reduce((pre,cur)=>pre+cur,0)
+    count += all
 }
